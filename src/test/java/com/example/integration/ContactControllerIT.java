db5d7d1b852a6437c01f0a7ebb73c74d05f65fce
bcrosby <bcrosby@staff.iinet.net.au>
2015-12-18 12:16:35 +1030

    Adding integration test.

--- src/test/java/com/example/integration/ContactControllerIT.java
+++ src/test/java/com/example/integration/ContactControllerIT.java
@@ -1,17 +1,29 @@
 package com.example.integration;
 
 import com.example.Application;
+import com.example.domain.Contact;
+import com.example.domain.ContactRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -26,27 +38,67 @@ public class ContactControllerIT {
 
     private RestTemplate clientTemplate;
 
+    private static final String addContactUrl = "/add";
+    private static final String listContactsUrl = "/contacts";
+
+    @Autowired
+    ContactRepository contactRepository;
+
     @Before
     public void setUp() throws Exception {
 
         this.baseUrl = new URL("http://localhost:" + this.port);
         this.clientTemplate = new TestRestTemplate();
+        contactRepository.deleteAll();
     }
 
-    public void testAdd() {
+    @Test
+    public void testAddNullContact() {
 
-
-
-    }
-
-    public void testContacts() {
-
-
-
+        Contact contact = null;
+        ResponseEntity<Contact> response = clientTemplate.postForEntity(baseUrl + addContactUrl, contact, Contact.class);
+        assertEquals(response.getStatusCode(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
 
+    @Test
+    public void testAddValidContact() {
 
+        Contact contact = new Contact("Alex", "Wessley", "alex.w@gmail.com");
+        ResponseEntity<Contact> response = clientTemplate.postForEntity(baseUrl + addContactUrl, contact, Contact.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(response.getBody());
+        assertEquals("Alex", response.getBody().getFirstName());
+        assertEquals("Wessley", response.getBody().getLastName());
+        assertEquals("alex.w@gmail.com", response.getBody().getEmailAddress());
+    }
 
+    @Test
+    public void testContactsEmpty() {
+
+        ResponseEntity<List> response = clientTemplate.getForEntity(baseUrl + listContactsUrl, List.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(response.getBody());
+        assertTrue(response.getBody().isEmpty());
+    }
+
+    @Test
+    public void testContactsReturned() {
+
+        // add a few contacts here
+        Contact contact1 = new Contact("Alex", "Wessley", "alex.w@gmail.com");
+        Contact contact2 = new Contact("Jason", "Manson", "jason.m@gmail.com");
+        Contact contact3 = new Contact("Paul", "Linders", "paul.l@gmail.com");
+
+        clientTemplate.postForEntity(baseUrl + addContactUrl, contact1, Contact.class);
+        clientTemplate.postForEntity(baseUrl + addContactUrl, contact2, Contact.class);
+        clientTemplate.postForEntity(baseUrl + addContactUrl, contact3, Contact.class);
+
+        ResponseEntity<List> response = clientTemplate.getForEntity(baseUrl + listContactsUrl, List.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(response.getBody());
+        assertFalse(response.getBody().isEmpty());
+        assertEquals(3, response.getBody().size());
+    }
 
 }
